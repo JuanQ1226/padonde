a8c113e0161f9d06b28f2ea4eb8ce22063c07c4c
JuanQ1226 <juan.quintana5@upr.edu>
2022-12-02 13:09:06 -0400

    Added Map to website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import mapboxgl from 'mapbox-gl'; //enlist-disable-line import/no-webpack-loader-syntax
+import React from 'react';
+mapboxgl.accessToken = "pk.eyJ1IjoianVhbnExMjI2IiwiYSI6ImNsYjZxY2tnbzAzangzcG9keTA5OHluZGoifQ.gvrUPVXU_yyQnEDDy274ww";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Map creation
+
+
+export default class App extends React.PureComponent{
+  constructor(props) {
+    super(props);
+    this.state = {
+    lng: -70.9,
+    lat: 42.35,
+    zoom: 9
+    };
+    this.mapContainer = React.createRef();
+    }
+    componentDidMount() {
+      const { lng, lat, zoom } = this.state;
+      const map = new mapboxgl.Map({
+      container: this.mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
+      });
+      }
+      render() {
+        return (
+        <div>
+        <div ref={this.mapContainer} className="map-container" />
+        </div>
+        );
+        }
 }
-
-export default App;
